08af9dad39b0f5666c7b4a2debafb6b9f4232010
Usuario <Usuario@CPD02-M07>
2024-05-21 10:58:18 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleFuncoes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFuncoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solução -> System
+            //Função -> Bloco de códigos p/ executar uma determinada ação
+            //Classe -> Console
+
+            Console.WriteLine("Hi!!");
+            string nome = Console.ReadLine();
+            Console.WriteLine(nome);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
